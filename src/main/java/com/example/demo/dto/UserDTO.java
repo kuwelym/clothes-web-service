ccf13138de8c06d5384cc6abacb7f49a0c83181f
kuwelym <klyminh@gmail.com>
2024-04-03 14:38:52 +0700

    feat: validate token, fetch user details for Front-en

---
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String phoneNum;
+}
